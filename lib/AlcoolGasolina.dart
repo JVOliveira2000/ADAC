12320ca59e023661f828f2433538bd9c8074d6fe
João Vitor <joaovitor.mil2000@gmail.com>
2020-04-09 21:05:41 -0300

    commit on 'master'

--- lib/AlcoolGasolina.dart
+++ lib/AlcoolGasolina.dart
@@ -36,7 +36,7 @@ class _alcoolGasolinaState extends State<alcoolGasolina> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Alcool ou Gasolina"),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.green,
       ),
       body: Container(
         child: SingleChildScrollView(
